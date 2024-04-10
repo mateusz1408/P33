ed068a53fcb828f5eef89f5c49ed52e4ce1a1f5c
mateusz1408 <162459899+mateusz1408@users.noreply.github.com>
2024-04-10 13:33:50 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int x = 1;
+    int y = 1;
+
+    for(int i = 1; i < x; i++) {
+      for(int j = 1; j < y; j++) {
+         int a =(x*y);
+        System.out.print(a);
+        }
+         System.out.print(" ");
+        }
+      System.out.println("\t");
+    }
+    }
+    
